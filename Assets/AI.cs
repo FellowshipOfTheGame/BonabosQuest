4d39d00c834fee1106040bd7e41d570bd130749f
Guilherme Zanardo Borduchi <guilherme.borduchi@usp.br>
2015-08-08 15:55:33 -0300

    Movement and AI
    Character is moving, animating (kind of) properly, and enemies are
    following it.

---
+++ Assets/AI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+	private GameObject player;
+	private Rigidbody2D enemy;
+
+	public float maxSpeed = 2f;
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Character");
+		enemy = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveToPlayer();
+	}
+	
+	void moveToPlayer(){
+		float difX = player.transform.position.x - enemy.transform.position.x;
+		float difY = player.transform.position.y - enemy.transform.position.y;
+
+		float xSpeed, ySpeed;
+
+		if (difX < -0.1){
+			xSpeed = -maxSpeed;
+		}else if(difX > 0.1){
+			xSpeed = maxSpeed;
+		}else{
+			xSpeed = 0;
+		}
+
+		if(difY < -0.1){
+			ySpeed = -maxSpeed;
+		}else if(difY > 0.1){
+			ySpeed = maxSpeed;
+		}else{
+			ySpeed = 0;
+		}
+
+		enemy.velocity = new Vector2(xSpeed, ySpeed);
+	}
+}
---
+++ Assets/script/move.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum Directions {
+	IDLE,
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+}
+
+public class move : MonoBehaviour {
+	private Rigidbody2D player;
+	private Animator anim;
+
+	public float maxSpeed = 10f;
+	int faceDirection = (int) Directions.IDLE; 
+
+	void Start () {
+		player = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+
+	void FixedUpdate () {
+		float moveH = Input.GetAxis("Horizontal");
+		float moveV = Input.GetAxis("Vertical");
+
+		player.velocity = new Vector2(moveH * maxSpeed, moveV * maxSpeed);
+		anim.SetFloat("Speed", Mathf.Abs(moveH) + Mathf.Abs(moveV));
+
+		if(moveH > 0){
+			if(faceDirection != (int) Directions.RIGHT)
+				Flip();
+
+			faceDirection = (int) Directions.RIGHT;
+			anim.SetInteger("faceDirection", (int) Directions.RIGHT);
+		} else if(moveH < 0){
+			if(faceDirection != (int) Directions.LEFT)
+				Flip();
+
+			faceDirection = (int) Directions.LEFT;
+			anim.SetInteger("faceDirection", (int) Directions.LEFT);
+		}
+
+		if(moveV > 0){
+			faceDirection = (int) Directions.UP;
+			anim.SetInteger("faceDirection", (int) Directions.UP);
+		} else if(moveV < 0){
+			faceDirection = (int) Directions.DOWN;
+			anim.SetInteger("faceDirection", (int) Directions.DOWN);
+		}
+
+		if(moveH == 0 && moveV == 0){
+			faceDirection = (int) Directions.IDLE;
+			anim.SetInteger("faceDirection", (int) Directions.IDLE);
+		}
+
+
+	}
+
+	void Flip(){
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
